876cbe83348e7e827cd7b61fbbced9155c7b12cb
MahmiaVaideHi <mahmiavaidehi@gmail.com>
2024-05-08 19:48:15 +0530

    Minor Changes :-
    -Computer Choices
    -Animation

--- app.js
+++ app.js
@@ -3,61 +3,71 @@ let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
 const msg = document.querySelector("#msg");
-
 const userScorePara = document.querySelector("#user-score");
 const compScorePara = document.querySelector("#comp-score");
+const compSelectedImage = document.getElementById("comp-selected-image");
 
 const genCompChoice = () => {
-  const options = ["rock", "paper", "scissors"];
-  const randIdx = Math.floor(Math.random() * 3);
-  return options[randIdx];
+    const options = ["rock", "paper", "scissors"];
+    const randIdx = Math.floor(Math.random() * 3);
+    return options[randIdx];
 };
 
 const drawGame = () => {
-  msg.innerText = "Game was Draw. Play again.";
-  msg.style.backgroundColor = "#081b31";
+    msg.innerText = "Game was Draw. Play again.";
+    msg.style.backgroundColor = "#081b31";
 };
 
 const showWinner = (userWin, userChoice, compChoice) => {
-  if (userWin) {
-    userScore++;
-    userScorePara.innerText = userScore;
-    msg.innerText = `You win! Your ${userChoice} beats ${compChoice}`;
-    msg.style.backgroundColor = "green";
-  } else {
-    compScore++;
-    compScorePara.innerText = compScore;
-    msg.innerText = `You lost. ${compChoice} beats your ${userChoice}`;
-    msg.style.backgroundColor = "red";
-  }
+    const userHand = document.querySelector("#user-hand");
+    const compHand = document.querySelector("#comp-hand");
+    userHand.classList.remove("shake");
+    compHand.classList.remove("shake");
+
+    if (userWin) {
+        userScore++;
+        userScorePara.innerText = userScore;
+        msg.innerText = `You win! Your ${userChoice} beats ${compChoice}`;
+        msg.style.backgroundColor = "green";
+        userHand.classList.add("shake");
+    } else {
+        compScore++;
+        compScorePara.innerText = compScore;
+        msg.innerText = `You lost. ${compChoice} beats your ${userChoice}`;
+        msg.style.backgroundColor = "red";
+        compHand.classList.add("shake");
+    }
+
+    // Highlight computer's choice image
+    compSelectedImage.src = `./images/${compChoice}.png`;
 };
 
 const playGame = (userChoice) => {
-  //Generate computer choice
-  const compChoice = genCompChoice();
+    const compChoice = genCompChoice();
 
-  if (userChoice === compChoice) {
-    //Draw Game
-    drawGame();
-  } else {
-    let userWin = true;
-    if (userChoice === "rock") {
-      //scissors, paper
-      userWin = compChoice === "paper" ? false : true;
-    } else if (userChoice === "paper") {
-      //rock, scissors
-      userWin = compChoice === "scissors" ? false : true;
+    compSelectedImage.src = ""; // Reset computer's choice image
+
+    if (userChoice === compChoice) {
+        drawGame();
     } else {
-      //rock, paper
-      userWin = compChoice === "rock" ? false : true;
+        let userWin = true;
+        if (userChoice === "rock") {
+            userWin = compChoice === "paper" ? false : true;
+        } else if (userChoice === "paper") {
+            userWin = compChoice === "scissors" ? false : true;
+        } else {
+            userWin = compChoice === "rock" ? false : true;
+        }
+        showWinner(userWin, userChoice, compChoice);
     }
-    showWinner(userWin, userChoice, compChoice);
-  }
 };
 
 choices.forEach((choice) => {
-  choice.addEventListener("click", () => {
-    const userChoice = choice.getAttribute("id");
-    playGame(userChoice);
-  });
+    choice.addEventListener("click", () => {
+        const userChoice = choice.getAttribute("id");
+        playGame(userChoice);
+    });
 });
+
+mySound = new sound("click.mp3");
+mySound.play();
